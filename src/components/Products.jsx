02d27eaf2259fc471ceefc641822eae00b9adae4
MJaved9 <95mohdjaved@gmail.com>
2022-05-30 17:10:58 +0530

    a

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+import { Flex, Grid } from "@chakra-ui/react";
+// import axios from "axios";
+import AddProduct from "./AddProduct";
 
 const Products = () => {
   // TODO: Remove below const and instead import them from chakra
-  const Flex = () => <div />;
-  const Grid = () => <div />;
+  // const Flex = () => <div />;
+  // const Grid = () => <div />;
 
   return (
     <Flex>
+       <AddProduct/> 
       {/*  AddProduct */}
       <Grid>{/* List of Products */}</Grid>
       {/* Pagination */}
